d1c898212a1b57eefd95cb2a7f49667adc1d02ec
Brett Klooster <bklooster@yahoo.com>
2015-01-24 09:26:27 -0600

    Adding basic input to the app. No logic for if playing the correct note yet.

--- JingleBears/Assets/Scripts/Controller.cs
+++ JingleBears/Assets/Scripts/Controller.cs
@@ -23,12 +23,46 @@ public class Controller : MonoBehaviour {
 	}
 
 	void Start() { 
+		Staff.HidePlayingNote();
 		LoadTestSong();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(_curSong != null) { 
+			HandleInput();	
+		}
+	}
+
+	private void HandleInput() { 
+		//Check for Octave Higher
+		int curKey = -1;
+
+		if(Input.GetButton("C")) { 
+			curKey = 0;
+		} else if(Input.GetButton("D")) { 
+			curKey = 2;
+		} else if(Input.GetButton("E")) { 
+			curKey = 4;
+		} else if(Input.GetButton("F")) { 
+			curKey = 6;
+		} else if(Input.GetButton("G")) { 
+			curKey = 8;
+		} else if(Input.GetButton("A")) { 
+			curKey = 10;
+		} else if(Input.GetButton("B")) { 
+			curKey = 12;
+		}
+
+		if(curKey >= 0 && Input.GetButton("OctaveHigh")) { 
+			curKey += 16;
+		} 
+
+		if(curKey >= 0) { 
+			Staff.ShowPlayingNote(curKey);
+		} else { 
+			Staff.HidePlayingNote();
+		}
 	}
 
 
